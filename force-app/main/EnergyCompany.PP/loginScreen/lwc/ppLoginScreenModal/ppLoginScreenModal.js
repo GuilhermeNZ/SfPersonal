84ebebf3881e10b552c706f91ae7eb7801c9b1b3
GuilhermeNZ <guilhermenuneszwipp@hotmail.com>
2025-07-06 13:20:05 -0300

    dev(ppLoginScreenModal): added google repatchav2

--- force-app/main/EnergyCompany.PP/loginScreen/lwc/ppLoginScreenModal/ppLoginScreenModal.js
+++ force-app/main/EnergyCompany.PP/loginScreen/lwc/ppLoginScreenModal/ppLoginScreenModal.js
@@ -3,6 +3,7 @@ import { wire } from 'lwc';
 import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 
 import ENERGY_LOGIN_IMAGE from '@salesforce/resourceUrl/energyLoginImage';
+import pageUrl from '@salesforce/resourceUrl/recaptchaV2';
 
 import USER_ID from '@salesforce/user/Id';
 import USER_EMAIL_FIELD from "@salesforce/schema/User.Email";
@@ -17,6 +18,26 @@ export default class PpLoginScreenModal extends LightningModal {
 
     errorsMessageWhenRendering = '';
     errorHandledWhenRendering = false;
+
+    navigateTo;
+
+    constructor(){
+        super();
+        this.navigateTo = pageUrl;
+        window.addEventListener("message", this.listenForMessage);//add event listener for message that we post in our recaptchaV2 static resource html file.
+    }
+
+    captchaLoaded(event){
+        if(event.target.getAttribute('src') == pageUrl){
+            console.log('Google reCAPTCHA is loaded.');
+        } 
+    }
+
+    listenForMessage(message){
+        console.log('message : ' + JSON.stringify( message ) );//message.data - The object passed from the other window.
+        console.log('message data : ' + message.data);//message.data - The object passed from the other window.
+        console.log('message origin : ' + message.origin);//message.origin - The origin of the window that sent the message at the time postMessage was called.
+    }
 
     @wire( getRecord, { recordId: USER_ID, fields: [USER_EMAIL_FIELD, USER_PASSWORD_FIELD] } )
     wiredUser( { error, data } ) {
@@ -45,6 +66,11 @@ export default class PpLoginScreenModal extends LightningModal {
                 this.handleError( this.errorsMessageWhenRendering );
             }, 0);
         }
+
+        var divElement = this.template.querySelector('div.recaptchaCheckbox');
+        //valide values for badge: bottomright bottomleft inline
+        var payload = {element: divElement, badge: 'bottomright'};
+        document.dispatchEvent(new CustomEvent("grecaptchaRender", {"detail": payload}));
     }
 
     handleSignIn() {
